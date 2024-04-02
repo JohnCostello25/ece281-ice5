ec81c8792a2466f0af6c3fa9730dabaf73a44696
JohnCostello25 <142033414+JohnCostello25@users.noreply.github.com>
2024-04-02 09:07:08 -0600

    first push

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -96,20 +96,24 @@ begin
 
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	-- Next State Logic
-    f_Q_next <= <state> when (<condition>) else -- going up
-                ...
-                ...
-                ... -- going down
-                ...
-                ... else
-                ...; -- default case
+    f_Q_next <= s_floor2 when ((i_up_down = '1') and (f_Q = s_floor1)) else -- going up
+                s_floor3 when ((i_up_down = '1') and (f_Q = s_floor2)) else
+                s_floor4 when ((i_up_down = '1') and (f_Q = s_floor3)) else
+                
+                s_floor3 when ((i_up_down = '0') and f_Q = s_floor4) else -- going down
+                s_floor2 when ((i_up_down = '0') and f_Q = s_floor3) else
+                s_floor1 when ((i_up_down = '0') and f_Q = s_floor2) else
+                
+                f_Q; -- default case
   
 	-- Output logic
     with f_Q select
-        o_floor <= <value> when s_floor1,
-                ...
-                ...
-                <value> when others; -- default is floor 2
+        o_floor <= "0001" when s_floor1,
+                   "0010" when s_floor2,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   
+                   "0010" when others; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
@@ -118,10 +122,19 @@ begin
 	register_proc : process (i_clk)
     begin
          -- synchronous reset
+        if rising_edge(i_clk) then 
         
-        -- if elevator is enabled, advance floors
-        -- if not enabled, stay at current floor
-    
+            if i_reset = '1' then
+                f_Q <= s_floor2;
+            
+            -- if elevator is enabled, advance floors
+            elsif i_stop = '0' then
+                f_Q <= f_Q_next;
+            
+            -- if not enabled, stay at current floor
+            end if;
+         end if;
+         
 	end process register_proc;	
 	
 	-------------------------------------------------------------------------------------------------------
